b26bd7a797b19ec64470b0c1a9365c8291f9eee8
George Karampelas <george.karampelas.26@gmail.com>
2021-12-04 20:43:13 +0200

    inputs

---
+++ project_1/Exercise5/logIn.js
@@ -0,0 +1,14 @@
+window.onload = function()
+{
+    console.log("loaded")
+
+
+
+}
+
+function onClickSubmit(){
+    console.log("Submit...")
+    let cvv = document.getElementById("card-CVV")
+    console.log(cvv.value)
+   
+}
